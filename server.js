fd708d2b7bdf981ccf235b4ff98fb277381c5a0f
Emily Stewart <stewartemilym2014@gmail.com>
2021-04-16 14:04:06 -0700

    fixed post and get routing, also added my delete route

--- server.js
+++ server.js
@@ -1,7 +1,17 @@
 const fs = require('fs');
+const express = require('express');
 const path = require('path');
 const PORT = process.env.PORT || 3001;
 const app = express();
+const router = require('express').Router();
+const shortid = require('shortid');
+let noteObj = require('./db/db.json');
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public'));
+// app.use('/api/public/notes.html');
+// app.use('/public/index.html');
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, './public/index.html'));
@@ -9,9 +19,40 @@ app.get('/', (req, res) => {
 app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, './public/notes.html'));
   });
+app.get('/api/notes', (req, res) => {
+    res.json(noteObj);
+});
+app.post('/api/notes',(req, res) => {
+  console.log(req.body);
+  const newPost = {
+    id: shortid.generate(),
+    title: req.body.title,
+    text: req.body.text
+  }
+  console.log(newPost);
+  noteObj.push(newPost);
+  fs.writeFile('./db/db.json',JSON.stringify(noteObj),(err) =>{
+    if (err) {
+      throw err;
+    }
+    res.json(noteObj);
+  })
+})
+app.delete('/api/notes/:id',(req,res) => {
+   console.log(req.params);
+    noteObj = noteObj.filter((notesX) => notesX.id !==req.params.id );
+   console.log(noteObj);
+   fs.writeFile('./db/db.json',JSON.stringify(noteObj), (err) =>{
+     if (err) {
+       throw err;
+     }
+     res.status(200).json(noteObj);
+   })
+})
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, './public/index.html'));
   });
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
   });
+  module.exports = router;
